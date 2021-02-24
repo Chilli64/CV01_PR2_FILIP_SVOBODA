ce42744dcfcf84d05a85cd95b77280d2459a338a
Filip Svoboda <st58611@upce.cz>
2021-02-24 22:23:25 +0100

    Přidejte soubory projektu.

---
+++ CV01_PR2_FILIP_SVOBODA/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CV01_PR2_FILIP_SVOBODA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char Pismeno = 'a';
+            while (Pismeno != ('z' + 1))
+            {
+                Console.WriteLine(Pismeno);
+                Pismeno++;
+            }
+        }
+    }
+}
